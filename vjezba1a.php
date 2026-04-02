38b89fb3a88e455e5785d8d2b4868b432f4a649e
SvenGrgic <167447139+SvenGrgic@users.noreply.github.com>
2026-04-02 15:00:21 +0200

    Prva vježba
    Prva vježba za php

---
+++ vjezba1a.php
@@ -0,0 +1,15 @@
+<?php 
+$naslov = "Moj prvi PHP dokument";
+$autor = "Sven Grgić";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je <strong>$autor</strong>.<p>";
+echo '<a href="https://www.index.hr" target="_blank">Posjeti INDEX.hr</a>';
+?>
+<!doctype html>
+<html>
+<body>
+    <!-- Naziv datoteke: vjezba1a.php -->
+</body>
+</html>
+
